28597f61c37aa7dfc98059bf3bc11dd02ff6bb33
Muzammil ahmad <105576971+Muzammilahmad12@users.noreply.github.com>
2024-05-05 15:59:09 +0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
