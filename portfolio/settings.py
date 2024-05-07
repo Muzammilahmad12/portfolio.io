edf9225daffb80afddbff09265adaf46633e8c17
Muzammil ahmad <105576971+Muzammilahmad12@users.noreply.github.com>
2024-05-07 18:42:30 +0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,6 +27,25 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+CSRF_TRUSTED_ORIGINS = ['portfolioio-production.up.railway.app']
+
+CORS_REPLACE_HTTPS_REFERER = True
+
+CSRF_COOKIE_DOMAIN = 'bluemix.net'
+
+CORS_ORIGIN_WHITELIST = (
+    'portfolioio-production.up.railway.app',
+    'front.bluemix.net',
+    'bluemix.net',
+)
+
 
 # Application definition
 
